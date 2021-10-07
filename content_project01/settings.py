7b0b469301e039466077b2ee6517d9510529ad7b
JARD <armando310388@hotmail.com>
2021-10-07 17:21:20 -0500

    Hide key

--- content_project01/settings.py
+++ content_project01/settings.py
@@ -22,7 +22,9 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'basic_app/templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hqud05!1e#(f!y!986sq%6n-891!hl5!_99dvca59!h=5hq=nr'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
